d2415ec2624421015ff8b37552a81e802b486d2a
NicksCodez <elisei.nicolae08@gmail.com>
2023-03-21 15:56:50 +0200

    implement dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './style.css';
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+  const titleButton = dropdown.querySelector('.dropdown-title > button');
+
+  const menuButtons = dropdown.querySelectorAll('.dropdown-list-item > button');
+
+  function showMenu(dropdownSelected) {
+    const menu = dropdownSelected.querySelector('.dropdown-list');
+    const caret = dropdownSelected.querySelector('.caret');
+    menu.classList.toggle('active');
+    caret.classList.toggle('caret-rotate');
+  }
+
+  function closeOtherMenus() {
+    const menus = document.querySelectorAll('.dropdown-list');
+    menus.forEach((menusItem) => {
+      if (
+        menusItem.classList.contains('active') &&
+        menusItem.closest('.dropdown') !== dropdown
+      ) {
+        showMenu(menusItem.closest('.dropdown'));
+      }
+    });
+  }
+
+  function selectItem(menuButton) {
+    titleButton.textContent = menuButton.textContent;
+    showMenu(dropdown);
+  }
+
+  titleButton.addEventListener('click', () => {
+    closeOtherMenus();
+    showMenu(dropdown);
+  });
+
+  titleButton.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      showMenu(dropdown);
+    }
+  });
+
+  menuButtons.forEach((menuButton) => {
+    menuButton.addEventListener('click', () => {
+      selectItem(menuButton);
+    });
+    menuButton.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        selectItem();
+      }
+    });
+  });
+});
